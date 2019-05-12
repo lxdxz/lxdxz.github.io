d89138715dd6d233e1fdf46e3193f1dd0ad92ee9
lxdxz <49426124+lxdxz@users.noreply.github.com>
2019-05-12 12:04:59 +0800

    Add files via upload

---
+++ KEY.py
@@ -0,0 +1,13 @@
+from random import *
+bet = ['a','b','c','d','e','f','g','h',]
+core = ['#','%','@','+','*','{','=']
+num = [1,2,3,4,5,6,7,8,9]
+a = choice(bet)
+b = choice(core)
+c = choice(num)
+d = choice(bet)
+e = choice(core)
+f = choice(num)
+g = choice(core)
+h = choice(num)
+print(h,end=""),print(c,end=""),print(b,end=""),print(d,end=""),print(e,end=""),print(g,end=""),print(h,end=""),print(a,end="")
